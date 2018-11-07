c2fc4e3c0b81c418b47e8ebb00a5190a21e65853
abrikos <me@abrikos.pro>
2018-11-07 14:39:38 +0900

    Added Support for the experimental syntax 'decorators-legacy' by "npm run eject"

--- src/games/minesweeper/Minesweeper.js
+++ src/games/minesweeper/Minesweeper.js
@@ -2,22 +2,29 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { action, observable } from 'mobx';
 
-//@inject('store') @observer
+@inject('store') @observer
 class Minesweeper extends React.Component {
-	@observable todos = [];
+	@observable value = 0;
 
 	constructor (props) {
 		super(props);
 		this.props = props;
 		this.store = this.props.store;
 		if (this.props.location.pathname === '/admin/profile') this.props.history.push('/admin/profile/info');
-
+		this.value = 0;
 	}
+
+	@action clickhandler = val => {
+		this.value += val;
+	}
+
 
 	render () {
 		return (
 			<div className="admin-profile">
-				Minesweeper
+				{this.value}
+				<button onClick={()=>this.clickhandler(1)} value='+'>+</button>
+				<button onClick={()=>this.clickhandler(-1)} value='-'>-</button>
 			</div>
 		);
 	}
